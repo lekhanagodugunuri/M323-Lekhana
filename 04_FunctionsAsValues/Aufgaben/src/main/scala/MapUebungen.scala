3f038564c968db196ba602c2e5bbb79d9ff7ccd8
lekhanagodugunuri <lekhana.godugunuri@rsrg.com>
2024-04-15 10:19:49 +0200

    working on tasks

--- 04_FunctionsAsValues/Aufgaben/src/main/scala/MapUebungen.scala
+++ 04_FunctionsAsValues/Aufgaben/src/main/scala/MapUebungen.scala
@@ -2,10 +2,13 @@ object MapUebungen {
 
   def main(args: Array[String]): Unit = {
     doubleNumbers(numbers);
-    convertFirstLetterUpperCase(words);
+    convertLetterUpperCase(words);
     halfNumber(numbers2);
+    formatiereAdressen(adressen).foreach(println);
+    extrahiereVornamenInGrossbuchstaben(namen).foreach(println);
 
   }
+  // Übung 1
   def doubleNumbers(numbers: List[Int]): List[Int] = {
     numbers.map(x => x * 2)
   }
@@ -13,13 +16,15 @@ object MapUebungen {
   val doubledNumbers = doubleNumbers(numbers)
   println(doubledNumbers)
 
-  def convertFirstLetterUpperCase (words : List[String]): List[String] = {
+  // Übung 2
+  def convertLetterUpperCase (words : List[String]): List[String] = {
     words.map(x => x.toUpperCase())
   }
   val words = List("Alice", "Bob", "Charlie")
-  val uppercaseWords = convertFirstLetterUpperCase(words)
+  val uppercaseWords = convertLetterUpperCase(words)
   println(uppercaseWords)
 
+  // Übung 3
   def halfNumber(numbers2: List[Int]): List[Int] = {
     numbers2.map(x => x / 2)
   }
@@ -27,11 +32,26 @@ object MapUebungen {
   val halfNumbers = halfNumber(numbers2)
   println(halfNumbers)
 
+// Übung 4
+  case class Adresse(strasse: String, hausnummer: Int, postleitzahl: String, stadt: String)
 
+  val adressen = List(
+    Adresse("Hauptstrasse", 10, "12345", "Musterstadt"),
+    Adresse("Nebenstrasse", 5, "23456", "Beispielburg")
+  )
 
+  val namen = List("Max Mustermann", "Erika Mustermann")
 
+  def formatiereAdressen(adressen: List[Adresse]): List[String] = {
+    adressen.map(adresse =>
+      s"${adresse.strasse} ${adresse.hausnummer}, ${adresse.postleitzahl} ${adresse.stadt}"
+    )
+  }
 
-
+  // Übung 5
+  def extrahiereVornamenInGrossbuchstaben(namen: List[String]): List[String] = {
+    namen.map(name => name.split(" ").head.toUpperCase)
+  }
 
 
 }
